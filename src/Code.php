c64b88bea2114528b472d3df25f53d6b8876c658
Jakub Frühbauer <jakub.fruhbauer@bohemiasoft.com>
2015-08-18 13:34:57 +0200

    Add validation ean is number, Rules for code format

--- src/Code.php
+++ src/Code.php
@@ -6,51 +6,63 @@ namespace EanCodeValidator;
  * @author Jakub Fruhbauer <jakub.fruhbauer@bohemiasoft.com>
  * @copyright (c) 2015, Bohemiasoft s.r.o. 14.8.2015
  */
-class Code {
+class Code
+{
 
-    /**
-     * @param string $code
-     * @param int $numbers
-     * @return boolean
-     */
-    public function isValidEan($code, $numbers) {
-        $status = FALSE;
+  /**
+   * @param string $code
+   * @param int $numbers
+   * @return boolean
+   */
+  public function isValidEan($code, $numbers)
+  {
+    $status = FALSE;
 
-        if (strlen($code) === $numbers) {
-            $modulo = $this->getModulo($code, $numbers);
-            $index = $numbers - 1;
-            $codeModulo = intval($code[$index]);
+    if (preg_match('{[0-9]}', $code))
+    {
+      if (strlen( $code ) === $numbers)
+      {
+        $modulo = $this->getModulo( $code, $numbers );
+        $index = $numbers - 1;
+        $codeModulo = intval( $code[$index] );
 
-            if ($codeModulo === $modulo) {
-                $status = TRUE;
-            }
+        if ($codeModulo === $modulo)
+        {
+          $status = TRUE;
         }
-
-        return $status;
+      }
     }
 
-    /**
-     * @param string $code
-     * @param int $numbers
-     * @return int
-     */
-    private function getModulo($code, $numbers) {
-        $string = strrev(substr($code, 0, -1));
-        $oddSum = 0;
-        $evenSum = 0;
+    return $status;
+  }
 
-        for ($i = 0; $i < $numbers - 1; $i++) {
-            if ($i % 2 === 0) {
-                $oddSum += $string[$i] * 3;
-            } elseif ($i % 2 === 1) {
-                $evenSum += $string[$i];
-            }
-        }
+  /**
+   * @param string $code
+   * @param int $numbers
+   * @return int
+   */
+  private function getModulo($code, $numbers)
+  {
+    $string = strrev( substr( $code, 0, -1 ) );
+    $oddSum = 0;
+    $evenSum = 0;
 
-        $calculation = ($oddSum + $evenSum) % 10;
-        $modulo = ($calculation === 0) ? 0 : 10 - $calculation;
-
-        return $modulo;
+    for ($i = 0; $i < $numbers - 1; $i++)
+    {
+      if ($i % 2 === 0)
+      {
+        $oddSum += $string[$i] * 3;
+      }
+      elseif ($i % 2 === 1)
+      {
+        $evenSum += $string[$i];
+      }
     }
+
+    $calculation = ($oddSum + $evenSum) % 10;
+    $modulo = ($calculation === 0) ? 0 : 10 - $calculation;
+
+    return $modulo;
+  }
 
 }
--- tests/UnitTest/EanTest.php
+++ tests/UnitTest/EanTest.php
@@ -6,28 +6,33 @@ namespace EanCodeValidator;
  * @author Jakub Fruhbauer <jakub.fruhbauer@bohemiasoft.com>
  * @copyright (c) 2015, Bohemiasoft s.r.o. 14.8.2015
  */
-class EanTest extends \PHPUnit_Framework_TestCase {
+class EanTest extends \PHPUnit_Framework_TestCase
+{
 
-    public function getEan() {
-        return array(
-            array('73513537', TRUE, 8),
-            array('1234567899874', FALSE, 13),
-            array('8414636079170', TRUE, 13),
-            array('0751747057474', TRUE, 13),
-        );
-    }
+  public function getEan()
+  {
+    return array(
+      array('73513537', TRUE, 8),
+      array('BV513537', FALSE, 8),
+      array('1234567899874', FALSE, 13),
+      array('ZA14636079170', FALSE, 13),
+      array('8414636079170', TRUE, 13),
+      array('0751747057474', TRUE, 13),
+    );
+  }
 
-    /**
-     * @covers \EanCodeValidator\Code::isValidEan
-     * @dataProvider getEan
-     * @param string $ean
-     * @param bool $isValid
-     * @param int $type
-     */
-    public function testEan($ean, $isValid, $type) {
-        $eanValidator = new \EanCodeValidator\Code();
+  /**
+   * @covers \EanCodeValidator\Code::isValidEan
+   * @dataProvider getEan
+   * @param string $ean
+   * @param bool $isValid
+   * @param int $type
+   */
+  public function testEan($ean, $isValid, $type)
+  {
+    $eanValidator = new \EanCodeValidator\Code();
 
-        $this->assertEquals($isValid, $eanValidator->isValidEan($ean, $type));
-    }
+    $this->assertEquals( $isValid, $eanValidator->isValidEan( $ean, $type ) );
+  }
 
 }
